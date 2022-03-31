72380bbfeb469aef5b23539581d80f7d9f633e65
najwanan <najwanan@hotmail.com>
2022-03-30 18:18:26 -0700

    added content for menu

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,10 +1,20 @@
 
 function createMenu() {
-
   const menuContent = `
-    
+    <h1>The Sweet Shop Menu</h1>
+    <h3>Food<h3>
+    <img src= "./Assets/cupcakes_unsplash.jpg"class="sweetImg"><img>
+    <img class="sweetImg"><img>
+    <h3>Drinks<h3>
+    <img class="sweetImg"><img>
  
-  `
+  `;
+
+  const contentDiv = document.querySelector("#content");
+  const menuDiv = document.createElement('div');
+
+  menuDiv.innerHTML = menuContent;
+  contentDiv.appendChild(menuDiv)
 
 } 
 

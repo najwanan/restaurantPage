1a5c311e39c8eb6dc16a2e24fc575f3ac39d2ba4
najwanan <najwanan@hotmail.com>
2022-04-05 23:25:07 -0700

    fixing img bug

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -5,7 +5,7 @@ function createMenu() {
     <h1 id="menuTitle">The Sweet Shop Menu</h1>
     <h3>Food<h3>
     <div class="menu">
-      <img src= "../../Assets/cupcakes_unsplash.jpg" class="sweetImg"/>
+      <img src = "../Assets/fresh-strawberry.jpg"class="sweetImg"/>
       
       <h3>Drinks<h3>
      

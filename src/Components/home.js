a4787b594e7b55695393ad6783dc5c1b54a9f919
najwanan <najwanan@hotmail.com>
2022-05-02 23:58:31 -0700

    changes to menu and home pages to troubleshoot jpeg bug

--- src/Components/home.js
+++ src/Components/home.js
@@ -4,7 +4,7 @@ function homepageContent() {
    
    const homePageText = 
     `<div class = "homeWrapper content">
-      <h1 id="homeTitle">Life is Short</h1>
+      <h1 id="homeTitle">Life is Short, Make it Sweet</h1>
 
          <p id="homeContent">Welcome to the Sweet Shop! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <div>
--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,11 +1,11 @@
-import cupcakes from './cupcakes_unsplash.jpg';
+import cupcakes from '../assets/cupcakes_unsplash.jpg';
 function createMenu() {
   const menuContent = `
     <div class = "menuDiv content">
     <h1 id="menuTitle">The Sweet Shop Menu</h1>
     <h3>Food<h3>
     <div class="menu">
-      <img src={cupcakes} />
+      <img />
       <h3>Drinks<h3>
       <div class= ".strawberry"></div>
     </div>
@@ -13,14 +13,15 @@ function createMenu() {
   `;
   /*const createPhoto = document.createElement('img')
   createPhoto.src = './tarts_unsplash.jpg'*/
-  const dessertImg = document.createElement('img')
+  const dessertImg = document.createElement(`img`); 
+  dessertImg.src = cupcake; 
   const contentDiv = document.querySelector("#content");
   const menuDiv = document.createElement('div');
 
   //dessertImg.setAttribute('src', '../assets/macaroons.jpg')
   menuDiv.innerHTML = menuContent;
   contentDiv.appendChild(menuDiv)
-  contentDiv.appendChild(cupcakes)
+  contentDiv.appendChild(dessertImg)
 
 } 
 
